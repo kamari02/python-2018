4fff831603f6a4c35921fc08018138394929a8bf
Kamari Maurquan Curry <kamari.curry@stu.jefferson.kyschools.us>
2018-09-24 09:33:03 -0400

    proj 2 dictionary

---
+++ doc1.py
@@ -0,0 +1,9 @@
+video_games = {
+    "title":"rainbow_6_siege",
+    "dev":"tom_clancy",
+    "genre":"shooter",
+    "year":"2015",
+    "operator":"6",
+    }
+mydict = {"a":"rainbow_6_siege", "B":"tom_clancy", "c":"shooter", "d":"2015"}
+mydict = {"e":"operators,ash,iq,thermite,rook,maverick,buck"}
